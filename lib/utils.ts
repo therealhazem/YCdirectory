6b6bed11c9a777dc96bc4fa9ca19305d28d082ef
therealhazem <140704105+therealhazem@users.noreply.github.com>
2025-09-24 03:08:07 +0300

    new updates

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,3 +13,7 @@ export function formateDate(date: string) {
     year: "numeric"
   })
 }
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
+}
